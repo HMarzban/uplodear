7c8f985bf90b9fdd991ad268da2ed11cc30b4a35
Hossein Marzban <marzban98@gmail.com>
2019-02-19 21:41:12 +0330

    Add ES6 version, some modification

--- ES5/script.js
+++ ES5/script.js
@@ -1,154 +1,146 @@
 "use strict"
-  var uploader = function(data){
+  var uploader = function(data) {
 
-  var configs = {
-    url: data.url || null,
-    el: data.el || "DropZon",
-    centerText : data.centerText || "drag and drop your files",
-    uploadProgress : data.uploadProgress || "Uploading is in progress...",
-    uploadFinish: data.uploadFinish || "Upload finish!",
-    uploadFaild: data.uploadFaild || "Uploading face to an Error!",
-    multiple: data.multiple || false,
-    before: data.before ,
-    after: data.after ,
-    progress: data.progress
-  }
-
-  var dropArea = document.getElementById(configs.el);
-  var inputFile = document.createElement("input");
-  var pinput  = document.createElement("p");
-  var progressbar = document.createElement("div");
-  inputFile.type = "file";
-  inputFile.style.display = "none"
-  //inputFile.accept = "image/*"
-  inputFile.multiple = configs.multiple;
-
-  pinput.textContent = configs.centerText;
-
-  progressbar.className = "progressBar";
-
-  var appendDOM = [progressbar, inputFile, pinput];
-  appendDOM.forEach(function(el){
-    dropArea.appendChild(el);
-  })
-
-  // Prevent default drag behaviors
-  ;['dragenter', 'dragover', 'dragleave', 'drop'].forEach( function(eventName) {
-    dropArea.addEventListener(eventName, preventDefaults, false)   
-    document.body.addEventListener(eventName, preventDefaults, false)
-  });
-
-  // Handle dropped files
-  dropArea.addEventListener('drop', handleDrop, false);
-  dropArea.addEventListener("click", dropAreaClick , false);
-  inputFile.addEventListener('change', function(){ handleFiles(this.files) }, false)
-
-  function dropAreaClick(){ 
-      inputFile.click();
-  }
-
-  function preventDefaults (e) {
-    e.preventDefault();
-    e.stopPropagation();
-  }
-
-  function handleDrop(e) {
-    var dt = e.dataTransfer;
-    var files = dt.files;
-    handleFiles(files);
-  }
-
-  function handleFiles(files) {
-    var fileList = []
-    Object.keys(files).forEach(function(index){
-      fileList.push(files[index])
-    })
-    initializeProgress(fileList.length);
-    fileList.forEach(uploadFile);
-  }
-
-  let uploadProgress = [];
-
-  function initializeProgress(numFiles) {
-    progressbar.style.display = "block" 
-    uploadProgress = []
-    for(let i = numFiles; i > 0; i--) {
-      uploadProgress.push(0)
+    var configs = {
+      url: data.url || null,
+      el: data.el || "DropZon",
+      centerText : data.centerText || "drag and drop your files",
+      uploadingProgress : data.uploadingProgress || "Uploading is in progress...",
+      uploadFinish: data.uploadFinish || "Upload finish!",
+      uploadFaild: data.uploadFaild || "Uploading face to an Error!",
+      multiple: data.multiple || false,
+      before: data.before ,
+      after: data.after ,
+      progress: data.progress
     }
-  }
-  
-  function updateProgress(fileNumber, percent) {
 
-    uploadProgress[fileNumber] = percent;
+    var uploadProgress = [];
 
-    var reduce = uploadProgress.reduce(function(tot, curr) { 
-    return tot + curr
+    var dropArea = document.getElementById(configs.el);
+    var inputFile = document.createElement("input");
+    var pinput  = document.createElement("p");
+    var progressbar = document.createElement("div");
+    inputFile.type = "file";
+    inputFile.style.display = "none";
+    //inputFile.accept = "image/*"
+    inputFile.multiple = configs.multiple;
+    pinput.textContent = configs.centerText;
+    progressbar.className = "progressBar";
+
+    var appendDOM = [progressbar, inputFile, pinput];
+    appendDOM.forEach(function(el){
+      dropArea.appendChild(el);
+    })
+
+    // Prevent default drag behaviors
+    ;['dragenter', 'dragover', 'dragleave', 'drop'].forEach( function(eventName) {
+      dropArea.addEventListener(eventName, preventDefaults, false)   
+      document.body.addEventListener(eventName, preventDefaults, false)
     });
 
-    var total = Math.floor( reduce  / uploadProgress.length );
+    // Handle dropped files
+    dropArea.addEventListener('drop', handleDrop, false);
+    dropArea.addEventListener("click", dropAreaClick , false);
+    inputFile.addEventListener('change', function(){ handleFiles(this.files) }, false)
 
-    // Progress Callback
-    if(configs.progress)
-      configs.progress(total, uploadProgress);
+    function dropAreaClick(){ 
+        inputFile.click();
+    } // @Function: dropAreaClick()
 
-    progressbar.style.width = total+"%";
-    progressbar.innerText = total+"%"
-    pinput.textContent = configs.uploadProgress;
+    function preventDefaults (event) {
+      event.preventDefault();
+      event.stopPropagation();
+    } // @Function: preventDefaults()
 
-  }
+    function handleDrop(event) {
+      var files = event.dataTransfer.dt.files;
+      handleFiles(files);
+    } // @Function: handleDrop()
 
-  function uploadFile(file, i) {
-    var url = configs.url;
+    function handleFiles(files) {
+      var fileList = []
+      Object.keys(files).forEach(function(index){
+        fileList.push(files[index])
+      })
+      initializeProgress(fileList.length);
+      fileList.forEach(uploadFile);
+    } // @Function: handleFiles()
+
+    function initializeProgress(numFiles) {
+      progressbar.style.display = "block";
+      uploadProgress = [];
+      for(var i = numFiles; i > 0; i--) {
+        uploadProgress.push(0);
+      }
+    } // @Function: initializeProgress()
     
-    var xhr = new XMLHttpRequest();
-    var formData = new FormData();
-    xhr.open('POST', url, true);
-    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    function updateProgress(fileNumber, percent) {
 
-    xhr.upload.addEventListener("progress", function(e) {
-      updateProgress(i, (e.loaded * 100.0 / e.total) || 100);
-    });
+      uploadProgress[fileNumber] = percent;
 
-
-    xhr.addEventListener('readystatechange', function(e) {
-
-      if (xhr.readyState == 4 && xhr.status == 200) {
-        progressbar.style.display = "none";
-        pinput.textContent = configs.uploadFinish;
-      }
-      else if (xhr.readyState == 4 && xhr.status != 200) {
-        // Error. Inform the user
-        progressbar.style.width = "0";
-        pinput.textContent = configs.uploadFaild
-      }
-
-      // after XHR callback
-      if(xhr.readyState == 4){
-        if(data.after)
-          data.after(xhr.responseText, xhr.status);
-      }
-    });
-
-    formData.append('file', file);
-    // Befor send XHR 
-
-    if(configs.before)
-      configs.before(function(){
-        xhr.send(formData);
+      var reduce = uploadProgress.reduce(function(tot, curr) { 
+        return tot + curr
       });
-    else
-      xhr.send(formData);
-   
-  }
+      
+      var total = Math.floor( reduce  / uploadProgress.length );
+      // Progress Callback
+      if(configs.progress)
+        configs.progress(total, uploadProgress);
 
-}
+      progressbar.style.width = total+"%";
+      progressbar.innerText = total+"%"
+      pinput.textContent = configs.uploadingProgress;
+
+    } // @Function: updateProgress()
+
+    function uploadFile(file, i) {
+      var url = configs.url;
+      
+      var xhr = new XMLHttpRequest();
+      var formData = new FormData();
+      xhr.open('POST', url, true);
+      xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+
+      xhr.upload.addEventListener("progress", function(e) {
+        updateProgress(i, (e.loaded * 100.0 / e.total) || 100);
+      });
+
+      xhr.addEventListener('readystatechange', function(e) {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+          progressbar.style.display = "none";
+          pinput.textContent = configs.uploadFinish;
+        }
+        else if (xhr.readyState == 4 && xhr.status != 200) {
+          // Error. Inform the user
+          progressbar.style.width = "0";
+          pinput.textContent = configs.uploadFaild
+        }
+        // after XHR callback
+        if(xhr.readyState == 4)
+          if(data.after)
+            data.after(xhr.responseText, xhr.status);
+      });
+
+      formData.append('file', file);
+
+      // Befor send XHR 
+      if(configs.before)
+        configs.before(function(){
+          xhr.send(formData);
+        });
+      else
+        xhr.send(formData);
+    
+    } // @Function: uploadFile()
+
+  } // @Function: uploader()
 
 
-
-
+console.time("time");
 var upload = new uploader({
   el : "DropZon",
   multiple: true,
+  centerText: "ES5, drag and drop your files",
   url: "https://api.cloudinary.com/v1_1/joezimim007/image/upload",
   before: function(done){
     setTimeout(function(){
@@ -164,7 +156,6 @@ var upload = new uploader({
     console.log(total,lists )
   }
 });
-
-
+console.timeEnd("time");
 
 
---
+++ ES6/script.js
@@ -0,0 +1,171 @@
+class uploader {
+
+  constructor(options) {
+
+    this.el                 = options.el;
+    this.url                = options.url;
+    this.multiple           = options.multiple;
+    this.centerText         = options.centerText;
+    this.uploadFaild        = options.uploadFaild;
+    this.before             = options.before;
+    this.after              = options.after;
+    this.progress           = options.progress;
+    this.centerText         = options.centerText || "drag and drop your files";
+    this.uploadingProgress  = options.uploadingProgress || "Uploading is in progress...";
+    this.uploadFinish       = options.uploadFinish || "Upload finish!";
+    this.uploadFaild        = options.uploadFaild || "Uploading face to an Error!";
+    this.multiple           = options.multiple || false;
+
+    this.uploadProgress     = [];
+
+    this.elements();
+  } // @constructor()
+
+
+  elements() {
+    this.$dropArea = document.getElementById(this.el);
+    const domList = [
+      {el:"input",name:"$inputFile"},
+      {el:"div",name:"$progressbar"},
+      {el:"p",name:"$pinput"}
+    ];
+    this.createDom(domList);
+    this.appendDOM();
+    this.domListener();
+  } // @Function: elements()
+
+  createDom = (element) => {
+    element.forEach(dom => this[`${dom.name}`] = document.createElement(dom.el) );
+    this.$inputFile.type = "file";
+    this.$inputFile.style.display = "none"
+    this.$inputFile.multiple = this.multiple;
+    this.$pinput.textContent = this.centerText;
+    this.$progressbar.className = "progressBar";
+  } // @Function: createDom()
+
+  appendDOM = () => {
+    // append can accept array of node dome
+    // more info: https://developer.mozilla.org/en-US/docs/Web/API/ParentNode/append
+    this.$dropArea.append(this.$inputFile, this.$progressbar, this.$pinput);
+  } // @Function: appendDOM()
+
+  domListener = () => {
+      // Prevent default drag behaviors
+      ;['dragenter', 'dragover', 'dragleave', 'drop'].forEach( (eventName) => {
+        this.$dropArea.addEventListener(eventName, event => this.preventDefaults(event), false) 
+        document.body.addEventListener(eventName, event => this.preventDefaults(event), false)
+      });
+      // Handle dropped files
+      this.$dropArea.addEventListener('drop', this.handleDrop, false);
+      this.$dropArea.addEventListener("click", () => this.$inputFile.click() , false);
+      this.$inputFile.addEventListener('change', event => this.handleFiles(event.target.files) , false)
+  } // @Function: domListener()
+
+  preventDefaults (event) {
+    event.preventDefault();
+    event.stopPropagation();
+  } // @Function: preventDefaults()
+
+  handleDrop(event) {
+    const files = event.dataTransfer.files;
+    this.handleFiles(files);
+  } // @Function: handleDrop()
+
+  handleFiles(files) {
+    files = [...files];
+    this.initializeProgress(files.length);
+    files.forEach(this.uploadFile);
+  } // @Function: handleFiles()
+
+  initializeProgress(numFiles) {
+    this.$progressbar.style.display = "block";
+    this.uploadProgress = [];
+    for(let i = numFiles; i > 0; i--) {
+      this.uploadProgress.push(0)
+    }
+  } // @Function: initializeProgress()
+  
+  updateProgress(fileNumber, percent) {
+    this.uploadProgress[fileNumber] = percent;
+    const total = Math.floor( this.uploadProgress.reduce((tot, curr) => tot + curr, 0) / this.uploadProgress.length);
+    // Progress Callback
+     if(this.progress)
+       this.progress(total, this.uploadProgress);
+    this.$progressbar.style.width = total+"%";
+    this.$progressbar.innerText = total+"%"
+    this.$pinput.textContent = this.uploadingProgress;
+  } // @Function: updateProgress()
+
+  uploadFile = (file, i) => {
+
+    var url = this.url;
+    var xhr = new XMLHttpRequest();
+    var formData = new FormData();
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+
+    xhr.upload.addEventListener("progress", (event) => {
+      this.updateProgress(i, (event.loaded * 100.0 / event.total) || 100);
+    });
+
+
+    xhr.addEventListener('readystatechange', () =>  {
+
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        this.$progressbar.style.display = "none";
+        this.$pinput.textContent = this.uploadFinish;
+      }else if (xhr.readyState == 4 && xhr.status != 200) {
+        // Error. Inform the user
+        this.$progressbar.style.width = "0";
+        this.$pinput.textContent = this.uploadFaild
+      }
+
+      // after XHR callback
+       if(xhr.readyState == 4)
+         if(this.after)
+           this.after(xhr.responseText, xhr.status);
+      
+    });
+
+    formData.append('file', file);
+
+    // Befor send XHR 
+    if(this.before)
+      this.before(function(){
+        xhr.send(formData);
+      });
+    else
+      xhr.send(formData);
+   
+  } // @Function: uploadFile()
+
+} // @Class: uploader()
+
+
+
+console.time("time");
+var upload = new uploader({
+  el : "DropZon",
+  multiple: true,
+  centerText: "ES6, drag and drop your files",
+  url: "https://api.cloudinary.com/v1_1/joezimim007/image/upload",
+  before: function(done){
+    setTimeout(function(){
+      console.log("befor up");
+      done();
+    }, 1000);
+  },
+  after: function(data, status){
+    console.log("data done");
+    console.log(data, status)
+  },
+  progress: function(total, lists){
+    console.log(total,lists )
+  }
+});
+console.timeEnd("time");
+
+
+
+
+
